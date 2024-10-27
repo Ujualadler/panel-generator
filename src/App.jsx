03cfef08277856da12af9e45a79272ac764c3121
UjualK <ujualk2000@gmail.com>
2024-10-27 09:18:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Panel from "./panel";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+ function App() {
+  return (
+    <Router>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Panel />} />
+        <Route path="/:Id" element={<Panel />} />
+        {/* <Route path="/page" element={<Page />} /> */}
+ 
+      </Routes>
+    </Router>
+  );
+}
+
+
+
+
+export default App
